9dc04cb1c28c978757f15ca1343b601382e20f9a
Isaiah Rose <expert4minecraft02@gmail.com>
2023-04-11 10:22:45 -0400

    Updating | Environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { FirebaseComponentComponent } from './firebase-component/firebase-compon
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { ErrorComponent } from './error/error.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +21,8 @@ import { ErrorComponent } from './error/error.component';
     ErrorComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule
   ],
@@ -21,3 +30,40 @@ import { ErrorComponent } from './error/error.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AuthenticationService {
+  userData: Observable<firebase.User>;
+  
+  constructor(private angularFireAuth: AngularFireAuth) {
+  this.userData = angularFireAuth.authState;
+  }
+  
+  /* Sign up */
+  SignUp(email: string, password: string) {
+  this.angularFireAuth
+  .createUserWithEmailAndPassword(email, password)
+  .then(res => {
+  console.log('You are Successfully signed up!', res);
+  })
+  .catch(error => {
+  console.log('Something is wrong:', error.message);
+  });
+  }
+  
+  /* Sign in */
+  SignIn(email: string, password: string) {
+  this.angularFireAuth
+  .signInWithEmailAndPassword(email, password)
+  .then(res => {
+  console.log("You're in!");
+  })
+  .catch(err => {
+  console.log('Something went wrong:',err.message);
+  });
+  }
+  
+  /* Sign out */
+  SignOut() {
+  this.angularFireAuth
+  .signOut();
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDw9lTSqKMa9t0u3JqUKLfu1w-11KTjZkg",
+        authDomain: "spudnik1-e55f3.firebaseapp.com",
+        databaseURL: "https://spudnik1-e55f3-default-rtdb.firebaseio.com",
+        projectId: "spudnik1-e55f3",
+        storageBucket: "spudnik1-e55f3.appspot.com",
+        messagingSenderId: "811703284016",
+        appId: "1:811703284016:web:0bb3860d2dd67760b1d8f5",
+        measurementId: "G-GVC0E4K5PV"
+    }
+};
