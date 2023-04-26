d7b636aaf0bc2558bff52f5273b8415b1b0a15a5
Isaiah Rose <expert4minecraft02@gmail.com>
2023-04-26 19:29:59 -0400

    Updated Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/services/auth';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor(public authService: AuthService, private db: AngularFireDatabase) { }
 
+  checkFields() {
+    
+  }
+  saveData(inputValue: string, descrip: string, itemVal: string) {
+    const item = this.db.list(`/${this.authService.userData.uid}/items/`);
+    const itemInfo = this.db.list(`/${this.authService.userData.uid}/items/${this.authService.userData.itemVal}`)
+    item.push(inputValue).then((resp) => {
+      console.log(resp);
+    }).catch((error) => {
+      console.error(error);
+    })
+
+    itemInfo.push(descrip).then((resp) => {
+      console.log(resp);
+    }).catch((error) => {
+      console.error(error);
+    })
+
+    itemInfo.push(itemVal).then((resp) => {
+      console.log(resp);
+    }).catch((error) => {
+      console.error(error);
+    })
+  }
+  
+  ngOnInit() { }
 }
