03bc5852b7e1a59b8b08a5307a51b19b238a6b21
Isaiah Rose <expert4minecraft02@gmail.com>
2023-05-04 12:52:42 -0400

    Dashboard Fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../shared/services/auth';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/compat/database';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @Component({
@@ -9,6 +9,8 @@ import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  userId = this.authService.userData.uid
+
   constructor(
     public authService: AuthService, 
     private db: AngularFireDatabase) { }
@@ -19,30 +21,12 @@ export class DashboardComponent {
 
   tabVisible = false
   //saveData function params inputValue: string, descrip: string, itemVal: string
-  saveData(inputValue: string) {
-    const item = this.db.list(`/${this.authService.userData.uid}/items/`);
-    //const itemInfo = this.db.list(`/${this.authService.userData.uid}/items/${this.authService.userData.itemVal}`)
-    item.push(inputValue).then((resp) => {
-      console.log(resp);
-    }).catch((error) => {
-      console.error(error);
-    })
-    /*
-    REMOVED UNTIL FIXED
-
-    itemInfo.push(descrip).then((resp) => {
-      console.log(resp);
-    }).catch((error) => {
-      console.error(error);
-    })
-
-    itemInfo.push(itemVal).then((resp) => {
-      console.log(resp);
-    }).catch((error) => {
-      console.error(error);
-    })
-    */
+  saveData(itemTitle: string, itemDesc: string, itemValue: string) {
+    const items = this.db.list(`/Users/${this.userId}/Items`);
+    items.push({
+        ItemTitle: itemTitle,
+        ItemDescription: itemDesc,
+        ItemValue: itemValue,
+      });
   }
-  
-  ngOnInit() { }
 }
