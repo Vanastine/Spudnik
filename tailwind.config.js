4da14f096ec2f761f9391aaa0f24e0080a416c2d
Isaiah Rose <expert4minecraft02@gmail.com>
2023-05-17 10:52:09 -0400

    Wiggle Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [
 
