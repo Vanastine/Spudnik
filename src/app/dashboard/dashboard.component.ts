b985a00f92541b39076b4636330e63fe203ce0eb
Isaiah Rose <expert4minecraft02@gmail.com>
2023-06-06 00:22:02 -0400

    Dashboard Update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../shared/services/auth';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { openCloseTrigger } from 'environments/page-animation';
 import { ModalitemComponent } from 'app/modalitem/modalitem.component';
 import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,37 +16,62 @@ export class DashboardComponent {
   itemTitle: string = ""
   itemDesc: string = ""
   itemVal: string = ""
-  list: any
+  itemsRef: AngularFireList<any>
+  items: Observable<any[]>
   tabVisible = false
+  list: any
+  isShown = false
 
   constructor(
     public authService: AuthService,
     public db: AngularFireDatabase,
     public modalItemComp: ModalitemComponent,
-    public router: Router) { }
+    public router: Router) {
+      this.itemsRef = db.list(`/Users/${this.userId}/Items`)
+      this.items = this.itemsRef.snapshotChanges().pipe(map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val()}))));
+    }
 
   //saveData function params inputValue: string, descrip: string, itemVal: string
-  saveData(itemTitle: string, itemDesc: string, itemVal: string) {
-    const items = this.db.list(`/Users/${this.userId}/Items`);
-    items.push({
+  saveData(itemTitle: string, itemDesc: string, itemVal: string) {    
+    this.itemsRef.push({
       ItemTitle: itemTitle,
       ItemDescription: itemDesc,
       ItemValue: itemVal,
+      ID: Math.random(),
     });
-
-    this.retrieveData()
 
     this.itemTitle= ""
     this.itemDesc= ""
     this.itemVal= ""
   }
 
+  //Retrieves Items From User DB
   retrieveData() {
-    const items = this.db.list(`/Users/${this.userId}/Items`);
-    items.valueChanges().subscribe((data) => { this.list = data;});
+    const items = this.db.list(`/Users/${this.userId}/Items/`);
+    items.valueChanges().subscribe((data) => { this.list = data });
+    console.log(this.items)
   }
 
-  navToItem(title: any) {
-    this.router.navigate([`dashboard/${title}`])
+  //Updates Item Selected
+  updateItem(key: string, itemTitle: string, itemDesc: string, itemVal: string) {
+    this.itemsRef.update(key, {
+      ItemTitle: itemTitle,
+      ItemDesc: itemDesc,
+      ItemVal: itemVal
+    })
+  }
+
+  //Deletes Item Selected
+  deleteItem(key: string) {
+    this.itemsRef.remove(key)
+  }
+
+  navToItem(itemKey: any, item: any) {
+    this.router.navigate([`dashboard/${itemKey}`]);
+  }
+
+  //Toggles Visibility of Item
+  toggleShow() {
+    this.isShown != this.isShown
   }
 }
